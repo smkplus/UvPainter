92572b1044e27a498eca4c572719d3ca78fe14e0
smkplus <smkplus3d@gmail.com>
2018-01-22 07:57:48 +0330

    FirstCommit

---
+++ Assets/Scripts/Painter.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Collider))]
+public class Painter : MonoBehaviour
+{
+    public Material simpleInkPaintMat;
+
+    private Texture brushTexture; 
+    private Texture mainTexture;
+    private RenderTexture renderTexture;
+
+    private int mainTexturePropertyID;
+    private int paintUVPropertyID;
+    private int brushTexturePropertyID;
+    private int brushScalePropertyID;
+    private int brushColorPropertyID;
+
+    public float brushSize;
+
+
+
+    void Start()
+    {
+
+        mainTexturePropertyID = Shader.PropertyToID("_MainTex");
+        paintUVPropertyID = Shader.PropertyToID("_PaintUV");
+        brushTexturePropertyID = Shader.PropertyToID("_Brush");
+        brushScalePropertyID = Shader.PropertyToID("_BrushScale");
+        brushColorPropertyID = Shader.PropertyToID("_ControlColor");
+
+        mainTexture = simpleInkPaintMat.GetTexture(mainTexturePropertyID);
+        brushTexture = simpleInkPaintMat.GetTexture(brushTexturePropertyID);
+
+        renderTexture = new RenderTexture(mainTexture.width, mainTexture.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+
+        Graphics.Blit(mainTexture, renderTexture);
+
+        simpleInkPaintMat.SetTexture(mainTexturePropertyID, renderTexture);
+    }
+
+    void OnDestroy()
+    {
+        renderTexture.Release();
+        simpleInkPaintMat.SetTexture(mainTexturePropertyID, mainTexture); 
+    }
+
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+    
+                Paint(hitInfo.textureCoord);
+            }
+        }
+    }
+
+    private void Paint(Vector2 uv)
+    {
+        var renderTextureBuffer = RenderTexture.GetTemporary(renderTexture.width, renderTexture.height);
+        simpleInkPaintMat.SetVector(paintUVPropertyID, uv);
+        simpleInkPaintMat.SetTexture(brushTexturePropertyID, brushTexture);
+        simpleInkPaintMat.SetFloat(brushScalePropertyID, brushSize);
+        simpleInkPaintMat.SetVector(brushColorPropertyID, new Vector4(0, 128, 255, 128));
+
+        Graphics.Blit(renderTexture, renderTextureBuffer, simpleInkPaintMat);
+        Graphics.Blit(renderTextureBuffer, renderTexture);
+        RenderTexture.ReleaseTemporary(renderTextureBuffer); 
+    }
+}
---
+++ Assets/Shader/Paint.shader
@@ -0,0 +1,102 @@
+﻿Shader "Kamaly/PaintingUv"
+{
+	Properties{
+		_MainTex("MainTex", 2D) = "white" 
+		_BackGroundTex("BackGroundTex", 2D) = "white"
+		_Brush("Brush", 2D) = "white"
+		_BrushScale("BrushScale", FLOAT) = 0.1
+		_ControlColor("ControlColor", VECTOR) = (0, 0, 0, 0)
+		_PaintUV("Hit UV Position", VECTOR) = (0, 0, 0, 0)
+	}
+
+		SubShader{
+		CGINCLUDE
+		struct app_data {
+		float4 vertex:POSITION;
+		float4 uv:TEXCOORD0;
+	};
+
+	struct v2f {
+		float4 screen:SV_POSITION;
+		float4 uv:TEXCOORD0;
+	};
+
+	sampler2D _MainTex;
+	sampler2D _Brush;
+	sampler2D _BackGroundTex;
+	float4 _PaintUV;
+	float _BrushScale;
+	float4 _ControlColor;
+	ENDCG
+
+		// Pass BackGround
+		Pass{
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+		bool IsPaintRange(float2 mainUV, float2 paintUV, float brushScale) {
+		return
+			paintUV.x - brushScale < mainUV.x &&
+			mainUV.x < paintUV.x + brushScale &&
+			paintUV.y - brushScale < mainUV.y &&
+			mainUV.y < paintUV.y + brushScale;
+	}
+
+	v2f vert(app_data i) {
+		v2f o;
+		o.screen = UnityObjectToClipPos(i.vertex);
+		o.uv = i.uv;
+		return o;
+	}
+
+	float4 frag(v2f i) : SV_TARGET{
+		float h = _BrushScale;
+	float4 mainColor = tex2D(_MainTex, i.uv.xy);
+
+	float4 background = tex2D(_BackGroundTex, i.uv);
+	return background;
+	}
+		ENDCG
+	}
+
+
+		// Pass Brush
+		Blend SrcAlpha OneMinusSrcAlpha
+		Pass{
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#include "UnityCG.cginc"
+		bool IsPaintRange(float2 mainUV, float2 paintUV, float brushScale) {
+		return
+			paintUV.x - brushScale < mainUV.x &&
+			mainUV.x < paintUV.x + brushScale &&
+			paintUV.y - brushScale < mainUV.y &&
+			mainUV.y < paintUV.y + brushScale;
+	}
+
+	v2f vert(app_data i) {
+		v2f o;
+		o.screen = UnityObjectToClipPos(i.vertex);
+		o.uv = i.uv;
+		return o;
+	}
+
+	float4 frag(v2f i) : SV_TARGET{
+		float h = _BrushScale;
+	float4 mainColor = tex2D(_MainTex, i.uv.xy);
+	float4 brushColor = float4(1, 1, 1, 1);
+
+	if (IsPaintRange(i.uv, _PaintUV, h)) {
+		float2 uv = (i.uv - _PaintUV) / h * 0.5 + 0.5;
+		return  tex2D(_Brush,i.uv);
+	}
+	return mainColor;
+	}
+		ENDCG
+	}
+
+		
+	}
+}
